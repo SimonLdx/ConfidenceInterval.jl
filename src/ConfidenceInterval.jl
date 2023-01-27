29e512b3c6e8aa234c69f015b82debc0c50b35bc
SimonLdx <simon.landrieux@ens-paris-saclay.fr>
2023-01-27 15:26:26 +0100

    Initial code.

--- src/ConfidenceInterval.jl
+++ src/ConfidenceInterval.jl
@@ -1,5 +1,23 @@
 module ConfidenceInterval
 
-# Write your package code here.
+using Statistics
+using Distributions
+
+
+"Gives the Student coefficient corresponding to a confidence interval of `confidence` (∈ [0,1]) for a measurement repeated `n` times."
+function student(n,confidence)
+	return quantile(TDist(n-1), confidence+(1-confidence)/2)
+end
+
+"Returns the confidence interval corresponding to a confidence interval of `confidence` (∈ [0,1]) as 2-element vector for the list of measured values `meas`."
+function confidence_interval(meas, confidence)
+	l = length(meas)
+	avg = mean(meas)
+	dev = std(meas)
+	uncertainty = dev/l^.5
+	k = student(l, confidence)
+	broad_unc = k*uncertainty
+	return [avg-broad_unc, avg+broad_unc]
+end
 
 end
